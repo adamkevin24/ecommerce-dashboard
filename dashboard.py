f3f62f6215bd3814338cf1ffdc579ccb3d0e514c
Adam Kevin <149547395+adamkevin24@users.noreply.github.com>
2026-04-22 23:24:54 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ st.title("📊 Dashboard Analisis E-Commerce")
 # ======================
 @st.cache_data
 def load_data():
-    df = pd.read_csv('dashboard/main_data.csv')
+    df = pd.read_csv('2main_data.csv')
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     return df
 
